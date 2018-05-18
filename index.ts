b30a3c07e7d9b953a6cdd568ca37f1782a79e6d7
Rich Hildred <rhildred@gmail.com>
2018-05-17 21:34:19 -0400

    have a working enough example for class

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import express from 'express';
+
+// Create a new express application instance
+const app: express.Application = express();
+
+app.use(express.static("www"));
+
+app.get("/users/:uname", (req, res) => res.end("Hello " + req.params.uname));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
