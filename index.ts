e15dd6c7f012b85f438dbdbbaada2c91f7835436
Rich Hildred <rhildred@gmail.com>
2018-05-17 20:49:36 -0400

    got rid of a couple of things I wasn't ready for to scaffold new project

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-import readlineSync = require('readline-sync');
-
-console.log("Welcome");
-let userName: string = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+console.log("Hello World");
